26c1bb626a0005fd28c2847f3294238212f4adaf
AlajeTechnologies <127316671+AlajeTechnologies@users.noreply.github.com>
2023-05-04 04:00:46 +0100

    fixed app import in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'antd/dist/antd.css';
 import "bootstrap/dist/css/bootstrap.css"
-import App from 'App';
+import App from './App';
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <>
+  <React.StrictMode>
     <App />
-  </>,
+  </React.StrictMode>,
 )
