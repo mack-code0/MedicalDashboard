a3500181595e9cf1d6c5a9496f2dff5de8786414
AlajeTechnologies <127316671+AlajeTechnologies@users.noreply.github.com>
2023-05-04 03:53:57 +0100

    fixed app import in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import 'antd/dist/antd.css';
 import "bootstrap/dist/css/bootstrap.css"
+import App from 'App';
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
